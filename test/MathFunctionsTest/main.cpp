23c50d44e4c1da203b99f10d0e251c8a75f9866d
UCHIDA Masaru <usadamasa@gmail.com>
2016-08-19 20:03:43 +0900

    Run Test

---
+++ test/MathFunctionsTest/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by UCHIDA Masaru on 2016/08/19.
+//
+#include <gtest/gtest.h>
+
+#include "MathFunctions/Sqrt.h"
+
+TEST(SqrtTest, _25_to_5){
+    ASSERT_EQ(5.0, sandbox::Sqrt(25).GetSqrt());
+}
+
+TEST(SqrtTest, _100_to_10){
+    ASSERT_EQ(11.0, sandbox::Sqrt(100).GetSqrt());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
